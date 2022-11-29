b32ae69dad2577c4eee237e8af73453e4f67685c
edem <eddieblack159@gmail.com>
2022-11-29 22:43:24 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,7 @@ if last_digit > 5:
     print(f"{start} {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"{start} {number} is {last_digit} and is 0")
-else:
+elif last_digit < 6 and not 0:
     print(f"{start} {number} is {last_digit} and is less than 6 and not 0")
+else:
+    print(f"TypeError")
