97a73ec0ebf527e3fa394d22edc92395438ff347
edem <eddieblack159@gmail.com>
2022-12-01 23:17:38 +0000

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -11,3 +11,5 @@ def fizzbuzz():
             print("{}".format("Buzz"), end=" ")
         elif i % 3 == 0 and i % 5 == 0:
             print("{}".format("FizzBuzz"), end=" ")
+        else:
+            print("{}".format(i), end=" ")
