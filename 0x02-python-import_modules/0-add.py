08a4bc3a8b935e82dfdf1b71d47ec5d0dcb3e862
edem <eddieblack159@gmail.com>
2022-12-19 22:17:43 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import add_0 as addition
 
-a = 1
-b = 2
-
-if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, addition.add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
