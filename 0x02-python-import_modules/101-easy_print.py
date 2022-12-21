57329058a0b0cd9a07e541a4f87f898c1235294c
edem <eddieblack159@gmail.com>
2022-12-21 22:18:14 +0000

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-
-import builtins
-
-builtins.__dict__['_'] = lambda x: builtins.__dict__['print'](x, end="")
-
-_("#pythoniscool\n")
+import builtins; builtins.__dict__['print']("#pythoniscool\n", end="")
