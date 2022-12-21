17753e898e9d376cf3b0f0e530b4af97b6ebd062
edem <eddieblack159@gmail.com>
2022-12-21 14:25:08 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    arg_count = 0;
+
+    if len(sys.argv) < 2:
+        print("{:d} arguments.".format(0))
+    elif len(sys.argv) == 1:
+        print("{:d} argument:".format(1))
+        print("{:d}: {}".format(len(sys.argv), sys.argv[1]))
+    else:
+        for item in sys.argv:
+            arg_count += 1
+        print("{:d} arguments:".format(arg_count - 1))
+
+        for num in range(1, arg_count):
+            print("{:d}: {}".format(num, sys.argv[num]))
+
