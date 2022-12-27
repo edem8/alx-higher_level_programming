c31ff143dc3bba6291e8f308834250aefdcdeeb5
edem <eddieblack159@gmail.com>
2022-12-27 22:27:06 +0000

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    add = 0
+    div = 0
+    for tup in my_list:
+        for num in tup:
+            result += num
+        div += tup[1]
+    result = add / div
+    return result
