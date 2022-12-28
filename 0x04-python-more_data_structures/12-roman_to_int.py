c719ced1eaeb157678015da2c3fcc383660a1916
edem <eddieblack159@gmail.com>
2022-12-28 13:41:17 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,15 +10,14 @@ def roman_to_int(roman_string):
             'L': 50,
             'C': 100,
             'D': 500,
-            'M': 1000}
+            'M': 1000
+            }
 
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_values[roman_string[i]] >
-        roman_values[roman_string[i - 1]]:
+        if i > 0 and roman_values[roman_string[i]] > roman_values[roman_string[i - 1]]:
 
-            result += roman_values[roman_string[i]] - 2 *
-            roman_vlaues[roman_string[i - 1]]
+            result += roman_values[roman_string[i]] - 2 * roman_vlaues[roman_string[i - 1]]
         else:
             result += roman_values[roman_string[i]]
 
