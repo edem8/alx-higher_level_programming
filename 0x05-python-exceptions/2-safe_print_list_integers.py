4fcd301c2d6761d4d5e6fe06aad52d109a5b56f8
edem <eddieblack159@gmail.com>
2023-01-03 13:02:06 +0000

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,4 +11,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 continue
     except (NameError, TypeError, ValueError, IndexError):
         pass
+    print()
     return print_count
