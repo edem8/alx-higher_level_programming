65b5a530dd08cbd0f945977c2facb5f0d9264680
edem <eddieblack159@gmail.com>
2023-01-03 11:33:10 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x`=0):
+    try:
+        j = 0
+        for i in range(x + 1):
+            print(i, end="")
+            j += 1
+    except IndexError:
+        print()
+    return j
