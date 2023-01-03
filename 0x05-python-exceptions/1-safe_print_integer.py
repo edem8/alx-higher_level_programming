46f936d78ce6a27d81fccd2392ae698355027725
edem <eddieblack159@gmail.com>
2023-01-03 12:38:33 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         pass
     if isinstance(value, int):
         return True
