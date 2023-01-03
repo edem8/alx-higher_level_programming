e0d3bc0201f15dad2310689cf2bc50521125f86f
edem <eddieblack159@gmail.com>
2023-01-03 12:57:38 +0000

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    print_count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == type(i):
+                print("{:d}".format(my_list[i]), end="")
+                print_count += 1
+            else:
+                continue
+    except (NameError, TypeError, ValueError, IndexError):
+        pass
+    return print_count
