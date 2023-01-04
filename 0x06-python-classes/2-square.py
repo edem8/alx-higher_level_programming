c00453b3a594945d3c0a2403c2fc52a92dcbd3d0
edem <eddieblack159@gmail.com>
2023-01-04 23:27:16 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,9 @@ raise ValueError exception if size < 0
 class Square:
     """ Initialization method below"""
     def __init__(self, size=0):
-            if type(size) != int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
