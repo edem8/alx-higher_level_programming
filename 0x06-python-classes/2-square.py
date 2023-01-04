64fd811ee6a955bfe41e3c4b0f3c8005ef20658a
edem <eddieblack159@gmail.com>
2023-01-04 23:02:45 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A class based off 1-square.py
+
+Instantiation with optional size=0
+
+raise TypeError exception is size != type(int)
+raise ValueError exception if size < 0
+"""
+
+
+class Square:
+    """ Initialization method below"""
+    def __init__(self, size=0):
+        try:
+            if type(size) != int:
+                raise TypeError
+            if size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+        else:
+            self.__size = size
