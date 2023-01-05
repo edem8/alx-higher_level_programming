b84db0aaf26f3d52b5b6a89d3b531da1d80bc3a0
edem <eddieblack159@gmail.com>
2023-01-05 00:20:46 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,12 +7,7 @@ A class square based on 3-sqaure.py
 class Square:
     """ This class adds a getter and setter method """
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -20,7 +15,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
