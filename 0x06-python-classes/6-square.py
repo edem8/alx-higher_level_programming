288c98b8f048d19146b5317fbbca2d2e108c85f4
edem <eddieblack159@gmail.com>
2023-01-05 21:00:58 +0000

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A Sqaure class based on 5-square.py
+"""
+
+
+class Square:
+    """ Adds a private position instance attribute and its methods"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    if self.__position[1] == 0:
+                        for i in range(self.__position[0]):
+                            print(" ", end="")
+                    print("#", end="")
+                print()
