e7ff43bf5239f5cc0fff836bc141633a44950365
edem <eddieblack159@gmail.com>
2023-01-10 00:27:40 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A function that adds two integers, a and b.
+Example:
+
+>>> add_integer(2, 3) #doctest: +NOMARLIZE_WHITESPACES
+5
+
+"""
+
+
+def add_integer(a, b=98):
+    """ Addition function for integer and floats """
+    if not type(a) in [int, float]:
+        raise TypeError("a must be an integer")
+    if not type(b) in [int, float]:
+        raise TypeError("b must be an integer")
+    return a + b
