51232cebb51cb5d405d56585d5d4ee698a708e07
edem <eddieblack159@gmail.com>
2023-01-10 14:32:06 +0000

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """
-Function to say my  name 
+Function to say my name
+example
+
+    >>> say_my_name("Bless", "kwaku")
+    My name is Bless kwaku
 """
 
 
-def 
+def say_my_name(first_name, last_name=""):
+    """ A function that says my full name """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
