831a8d6ff848ba102eecf4913e218e272f0f17ed
edem <eddieblack159@gmail.com>
2023-01-11 11:29:46 +0000

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,13 +9,13 @@ void print_python_string(PyObject *p)
 	wchar_t *str = NULL;
 
 	printf("[.] string object info\n");
-	if (!PyUnicode_check(p))
+	if (!PyUnicode_Check(p))
 	{
 		printf(" [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	if (PyUnicodee_Is_COMPACT_ASCII(p))
+	if (PyUnicode_IS_COMPACT_ASCII(p))
 		type = "compact ascii";
 	else
 		type = "compact unicode object";
@@ -23,6 +23,6 @@ void print_python_string(PyObject *p)
 	str = PyUnicode_AsWideCharString(p, &len);
 
 	printf(" type: %s\n", type);
-	pritnf(" length: %ld\n", len);
+	printf(" length: %ld\n", len);
 	printf(" value: %ls\n", str);
 }
